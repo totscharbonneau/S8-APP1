d62607d2c3a2a180ded31d717b7adeea73565c7b
unknown <thomas.charbonneau16@gmail.com>
2025-09-05 10:26:00 -0400

    sigmoid and fullyconnected test pass

--- Problematique/dnn_framework/student/layers.py
+++ Problematique/dnn_framework/student/layers.py
@@ -9,19 +9,28 @@ class FullyConnectedLayer(Layer):
     """
 
     def __init__(self, input_count, output_count):
-        raise NotImplementedError()
+        # self.w = np.random.normal(loc=0.0,
+        #                   size=(output_count, input_count))
+        w = np.zeros((output_count,input_count))
+        b = np.zeros(output_count)
+        self.parameters = {"w":w,"b":b}
+
 
     def get_parameters(self):
-        raise NotImplementedError()
+        return self.parameters
 
     def get_buffers(self):
         raise NotImplementedError()
 
     def forward(self, x):
-        raise NotImplementedError()
+        y = x @ self.parameters['w'].T + self.parameters['b']
+        return (y, {"input":x, "output":y})
 
     def backward(self, output_grad, cache):
-        raise NotImplementedError()
+        deriv_W = output_grad.T @ cache["input"]
+        deriv_b = np.sum(output_grad, axis=0)
+        deriv_X = output_grad @ self.parameters["w"]
+        return (deriv_X, {"w":deriv_W,"b":deriv_b})
 
 
 class BatchNormalization(Layer):
@@ -63,10 +72,14 @@ class Sigmoid(Layer):
         raise NotImplementedError()
 
     def forward(self, x):
-        raise NotImplementedError()
+        y = 1/(1+np.exp(-x))
+        output = (y,{"input":x,"output":y})
+        return output
 
     def backward(self, output_grad, cache):
-        raise NotImplementedError()
+
+        deriv_sigm = cache["output"] * (1 - cache["output"])
+        return (deriv_sigm * output_grad , {})
 
 
 class ReLU(Layer):
